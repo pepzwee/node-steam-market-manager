a936ea9577650117363886fcb56e383b702ed171
Kristjan Kirpu <kristjan.kirpu@gmail.com>
2016-02-25 14:09:00 +0200

    added Resources
    Moved currencies and steam http responses to seperate resource files.

---
+++ resources/Currencies.js
@@ -0,0 +1,9 @@
+module.exports = {
+	"USD": 1,
+	"GBP": 2,
+	"EUR": 3,
+	"CHF": 4,
+	"RUB": 5,
+	"BRL": 7,
+	"JPY": 8
+};
---
+++ resources/ESteamResponse.js
@@ -0,0 +1,8 @@
+module.exports = {
+	"400": "We did a bad request. Please report this error at Github.",
+	"401": "We are not authorized to do this request. Please report this error at Github.",
+	"403": "Steam is refusing to respond.",
+	"500": "Steam is having issues. (Potential issue: No listings for this item.)",
+	"503": "Steam is having issues.",
+	"429": "We are making too many requests to Steam."
+};
